39af82cc6854ef412b03a794f6acf2571f6fd939
Pelumi <pelumi.olalekan.g@gmail.com>
2023-02-11 21:11:34 +0000

    task 1 back

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,30 +9,16 @@ if __name__ == '__main__':
     employeeId = sys.argv[1]
     baseUrl = "https://jsonplaceholder.typicode.com/users"
     url = baseUrl + "/" + employeeId
-    file_name = '{}.csv'.format(employeeId)
 
     response = requests.get(url)
-    employeeName = response.json().get('name')
+    username = response.json().get('username')
 
     todoUrl = url + "/todos"
     response = requests.get(todoUrl)
     tasks = response.json()
 
-#    csv_file = open('USER_ID.csv', 'w')
-#    csv_writer = csv.writer(csv_file)
-
-#    for task in tasks:
-#        row = []
-#        row.append(employeeId)
-#        row.append(employeeName)
-#        row.append(task.get('completed'))
-#        row.append(task.get('title'))
-#        csv_writer.writerow(row)
-
-#    csv_file.close()
-
-with open(file_name, 'w') as fd:
-    for task in tasks:
-        fd.write('"{}","{}","{}","{}"\n'
-                 .format(employeeId, employeeName,
-                         task.get('completed'), task.get('title')))
+    with open('{}.csv'.format(employeeId), 'w') as file:
+        for task in tasks:
+            file.write('"{}","{}","{}","{}"\n'
+                       .format(employeeId, username, task.get('completed'),
+                               task.get('title')))
