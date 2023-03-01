a04ebb2ac8b3dec4a1dbfe2a6e604a5b91260eda
Pelumi <pelumi.olalekan.g@gmail.com>
2023-03-01 01:28:07 +0000

    task 0 and 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Top Ten
+"""
+
+from requests import get
+import json
+
+def top_ten(subreddit):
+    """
+    a function that queries the Reddit API and prints the titles
+    of the first 10 hot posts listed for a given subreddit.
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        return None
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/top.json?limit=10'.format(subreddit)
+    response = get(url, headers=user_agent, allow_redirects=False)
+    results = response.json().get('data').get('children')
+
+
+    try:
+
+        for post in results:
+            print(post['data']['title'])
+    except Exception:
+        return None
+
+#if __name__ == '__main__':
+#    top_ten('dankmemes')
